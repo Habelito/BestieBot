48ed8bd75ea0d5a5de32e8bd8f2463fe9856b2e4
Habelito <79597197+Habelito@users.noreply.github.com>
2021-03-11 09:57:16 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+client.on('message', message => {
+	console.log(message.content);
+});
+
+client.login('your-token-goes-here');
